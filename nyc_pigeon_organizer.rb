d075faaeed8d421d22ac20135ea0ebbbd91e17f2
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 09:05:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,16 @@
 def nyc_pigeon_organizer(data)
  pigeon_list={}
- data.each{|prop_name, prop_value|
- 
- end
+ data.each{|prop_name, prop_values|
+  prop_values.each{|spec_value, pigeons|
+    pigeons.each{|name|
+      if pigeon_list.key?(name)==false
+        pigeon_list.store(name,{})      
+      end
+     if pigeon_list[name].key?(prop_name)==false
+       pigeon_list[name].store(prop_name)
+     end
+    }
+  }
  }
  
  return pigeon_list
